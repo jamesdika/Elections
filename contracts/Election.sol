3d665c6154422a837a82ca353445f3aa955c283c
jamesdika <jamesdika7@gmail.com>
2019-12-06 02:27:47 -0500

    First commit

---
+++ contracts/Election.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.12;
+
+contract Election {
+  //Store Candidate
+  //Read Candidate
+  string public candidate;
+  // Constructor
+  constructor() public {
+    candidate = "Candidate 1";
+  }
+}
